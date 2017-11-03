c5eab58b47b0955833019e0dd69f14d2a090da25
Guillaume-Batailler <guillaumedu59220@hotmail.com>
2017-11-03 12:42:45 +0100

    Aprés modification de 12:42

--- index.php
+++ index.php
@@ -6,7 +6,26 @@
 <body>
 
   <?php
-    echo "Bonjour, je suis un script PHP !";
+    $nom= ' Pèle';
+    $prénom='Sarah';
+    $age= '18';
+
+    echo "Bonjour ";
+    echo "<br>";
+    echo $prénom;
+    echo $nom;
+    echo "<br>";
+    echo  " j'ai $age ans";
+    echo "<br>";
+
+    if ($age>18) {
+        echo " majeur";
+    } elseif ($age>=15) {
+        echo " ado";
+    } else {
+        echo " enfant";
+    }
+    
   ?>
 
 </body>
