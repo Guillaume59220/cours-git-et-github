439cda55be6d30d40b292b2eee0ae57455483bbe
Guillaume-Batailler <guillaumedu59220@hotmail.com>
2017-11-03 15:19:32 +0100

    cours depuis 15:19

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     echo  " j'ai $age ans";
     echo "<br>";
 
-    if ($age>18) {
+    if ($age>=18) {
         echo " majeur";
     } elseif ($age>=15) {
         echo " ado";
@@ -26,6 +26,52 @@
         echo " enfant";
     }
     
+    $age2 = 33;
+    echo "<br>";
+    echo $age <=> $age2;
+    echo "<br>";
+    echo 1 <=> 2;
+    echo "<br>";
+    echo 2 <=> 1;
+    echo "<br>";
+
+    $a = null;
+
+    $c = 5;
+    $b = 2;
+    $d;
+
+    echo $a ?? $b ?? $c ?? $d;
+
+    //arithmetique
+
+    echo "<br>";
+    $a=12;
+    $b=$a+25;
+
+    echo "B vaut $b et A vaut $a";
+
+    echo "<br>";
+    $b = $a += 25;
+
+    echo "B vaut $b et A vaut $a";
+
+    echo "<br>";
+    $a++;
+    echo "A vaut $a <br>";
+
+    ++$a;
+    echo "A vaut $a <br>";
+
+    echo "avant A valais" .  ++$a;
+    echo "<br>";
+    echo "aprés A vaut" .  $a;
+    echo "<br>";
+    echo "avant A valais" .  $a++;
+    echo "<br>";
+    echo "aprés A vaut" .  $a;
+
+
   ?>
 
 </body>
